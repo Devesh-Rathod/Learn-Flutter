c28c514f71ed9062fcf957a7efcc97d243b69d4d
devesh <deveshrathod15@gmail.com>
2021-10-02 17:33:28 +0530

    Ok

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:lottie/lottie.dart';
-
 import 'HomeScreen.dart';
 import 'RegistrationScreen.dart';
 class LoginScreen extends StatefulWidget {
@@ -14,26 +12,46 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  // Form Key
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController emailController = new TextEditingController();
-  final TextEditingController passwordController = new TextEditingController();
+
+  // Text Editing Controllers (Email and password)
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // Firebase Authentication Instance
   final _auth = FirebaseAuth.instance;
   @override
+
   Widget build(BuildContext context) {
+    // SignUp function
+    Future <void> signIn(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      await _auth.signInWithEmailAndPassword(email: email, password: password).then((value) => {
+                Fluttertoast.showToast(msg: "Login Successful"),
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => HomeScreen())),
+              }).catchError((e) {
+        Fluttertoast.showToast(msg: e!.message);
+      });
+    }
+  }
+    // Email Field
     final emailField = TextFormField(
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
       validator: (value){
+        // If Empty
         if(value!.isEmpty){
-          return ("Please enter valid email id");
+            return ("Please enter valid email id");
         }
-        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-              .hasMatch(value)) {
+        // Regexpression for valid email 
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
             return ("Please Enter a valid email");
-          }
-          return null;
-        
+        }
+            // return null;
       },
       onSaved: (value){
         emailController.text = value!;
@@ -63,12 +81,12 @@ class _LoginScreenState extends State<LoginScreen> {
       onSaved: (value){
         passwordController.text = value!;
       },
-      textInputAction: TextInputAction.done,
+      textInputAction: TextInputAction.next,
        decoration: InputDecoration(
         prefixIcon: Icon(Icons.security),
         contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
         hintText: "Password",
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)) ,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
       ),
     );
     final loginButton = Material(
@@ -79,14 +97,15 @@ class _LoginScreenState extends State<LoginScreen> {
       child: MaterialButton(
         padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: MediaQuery.of(context).size.width,
-        onPressed: (){
-            signIn(emailController.text, passwordController.text);
-        }, 
         child: Text('Login', style: TextStyle(
               fontWeight: FontWeight.bold,
               color: Colors.white,
                ),
-            ), 
+            ),
+        onPressed: (){
+            signIn(emailController.text, passwordController.text);
+        }, 
+         
       )
     );
     return Scaffold(
@@ -146,18 +165,5 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-  void signIn(String email, String password) async {
-    if (_formKey.currentState!.validate()) {
-      await _auth
-          .signInWithEmailAndPassword(email: email, password: password)
-          .then((uid) => {
-                Fluttertoast.showToast(msg: "Login Successful"),
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => HomeScreen())),
-              })
-          .catchError((e) {
-        Fluttertoast.showToast(msg: e!.message);
-      });
-    }
-  }
+  
 }
